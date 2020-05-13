8ddca3274e36fbb816ec6c70f187d99df87bb366
ClaudiaHawkes <62360737+ClaudiaHawkes@users.noreply.github.com>
2020-05-13 19:32:07 +1200

    Tested Constuctors: Passed

--- BankAccountTest/UnitTest1.vb
+++ BankAccountTest/UnitTest1.vb
@@ -3,7 +3,24 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 <TestClass()> Public Class UnitTest1
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub TestContructors()
+
+        ' Arrange
+        Dim AccountHolder As String = "Ms I. N. Cognito"
+        Dim AccountNumber As String = "ABCD 890111 11167890"
+        Dim InterestRate As Double = 4.3
+        Dim Balance As Double = 10343.82
+        Dim CountryOfOrigin As String = "Isle of Man"
+
+        ' Act
+        Dim Account1 As New BankAccounts.BankAccount(AccountHolder, AccountNumber, Balance, InterestRate, CountryOfOrigin)
+        Dim Account2 As New BankAccounts.BankAccount(AccountHolder, AccountNumber, Balance, InterestRate)
+        Dim Account3 As New BankAccounts.BankAccount(AccountHolder, AccountNumber, Balance)
+
+        ' Assert
+        Assert.IsNotNull(Account1)
+        Assert.IsNotNull(Account2)
+        Assert.IsNotNull(Account3)
     End Sub
 
 End Class
---
+++ BankAccounts/BankAccount.vb
@@ -0,0 +1,38 @@
+﻿''' <summary>
+'''     Represents a single bank account.
+''' </summary>
+''' <author>Claudia Hawkes</author>
+''' <date>12/05/20</date>
+Public Class BankAccount
+
+    Private AccountHolder As String
+    Private AccountNumber As String
+    Private InterestRate As Double
+    Private Balance As Double
+    Private CountryOfOrigin As String
+
+    Public Sub New(Name As String, Account As String, Balance As Double, Interest As Double, Country As String)
+        Me.AccountHolder = AccountHolder
+        Me.AccountNumber = AccountNumber
+        Me.Balance = Balance
+        Me.InterestRate = InterestRate
+        Me.CountryOfOrigin = CountryOfOrigin
+
+    End Sub
+
+    Public Sub New(Name As String, Account As String, Balance As Double, Interest As Double)
+        Me.AccountHolder = AccountHolder
+        Me.AccountNumber = AccountNumber
+        Me.Balance = Balance
+        Me.InterestRate = InterestRate
+
+    End Sub
+
+    Public Sub New(Name As String, Account As String, Balance As Double)
+        Me.AccountNumber = AccountNumber
+        Me.AccountHolder = AccountHolder
+        Me.Balance = Balance
+    End Sub
+
+
+End Class
